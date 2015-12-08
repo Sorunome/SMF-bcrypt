1b077da73d01508ccf246be3da930f68d0170227
Sorunome <mail@sorunome.de>
2015-12-08 20:00:57 +0100

    start of rsa stuff

--- database.php
+++ database.php
@@ -9,4 +9,21 @@ $smcFunc['db_add_column']('{db_prefix}members',array(
 	'type' => 'text',
 	'null' => false
 ));
+
+$smcFunc['db_change_column']('{db_prefix}members',array(
+	'name' => 'passwd',
+	'type' => 'varchar',
+	'size' => 255,
+	'null' => false
+));
+
+
+$smcFunc['db_create_table']('{db_prefix}rsa_keys', array(
+	array('name' => 'key_id', 'type' => 'int', 'null' => false, 'auto' => true),
+	array('name' => 'privKey', 'type' => 'text', 'null' => false),
+	array('name' => 'pubKey', 'type' => 'text', 'null' => false),
+	array('name' => 'ts', 'type' => 'timestamp', 'null' => false)
+), array(
+	array('type' => 'primary', 'columns' => array('key_id'))
+));
 ?>
